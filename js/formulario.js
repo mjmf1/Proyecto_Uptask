c1111e7000fd07f4eb7d93261bccb2c499bd835e
mjmf1 <marcomora61@gmail.com>
2021-06-21 06:16:41 -0500

    Leyendo los datos del formulario de crear usuario con JavaScript y Validando

--- js/formulario.js
+++ js/formulario.js
@@ -1,1 +1,31 @@
-console.log('funciona !! ');
+EventListener();
+
+function EventListener (){
+    document.querySelector('#formulario').addEventListener('submit', validarRegistro);
+}
+
+function validarRegistro(e) {
+    e.preventDefault();
+    
+    var usuario = document.querySelector('#usuario').value,
+        password = document.querySelector('#password').value;
+
+        //console.log(usuario + " " + password); // comprobacion de envio formulario
+
+        if(usuario === '' || password === ''){
+            Swal({
+                type: 'error',
+                title: 'error...',
+                text: 'Ambos campos son obligatorios!',
+                //footer: '<a href="">Why do I have this issue?</a>'
+              })
+        }else{
+            Swal.fire({
+                type: 'success',
+                title: 'correcto!',
+                text: 'escribiste ambos campos!',
+                //footer: '<a href="">Why do I have this issue?</a>'
+              })
+        }
+
+}
