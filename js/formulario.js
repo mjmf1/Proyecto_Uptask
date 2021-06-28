398e3490dd5ce7d251c72510dc0344aea9297ce9
mjmf1 <marcomora61@gmail.com>
2021-06-27 19:28:33 -0500

    Enviando los datos del Formulario HTML mediante JavaScript a PHP

--- js/formulario.js
+++ js/formulario.js
@@ -8,11 +8,13 @@ function validarRegistro(e) {
     e.preventDefault();
     
     var usuario = document.querySelector('#usuario').value,
-        password = document.querySelector('#password').value;
+        password = document.querySelector('#password').value,
+        tipo = document.querySelector('#tipo').value;
 
         //console.log(usuario + " " + password); // comprobacion de envio formulario
 
         if(usuario === '' || password === ''){
+          // la validacion fallo
             Swal({
                 type: 'error',
                 title: 'error...',
@@ -20,12 +22,30 @@ function validarRegistro(e) {
                 //footer: '<a href="">Why do I have this issue?</a>'
               })
         }else{
-            Swal.fire({
-                type: 'success',
-                title: 'correcto!',
-                text: 'escribiste ambos campos!',
-                //footer: '<a href="">Why do I have this issue?</a>'
-              })
+           // ambos campos son correctos ejecutar ajax
+
+           //datos que se envian al servidor
+           var datos = new FormData();
+           datos.append('usuario', usuario);
+           datos.append('password', password);
+           datos.append('accion', tipo);
+
+           //console.log(datos.get('usuario'));
+
+           //crear el llamado ajax
+           var xhr = new XMLHttpRequest();
+
+           // abrir la conexion
+           xhr.open('POST', 'inc/modelos/modelos-admin.php', true);
+
+           //retorno de datos
+           xhr.onload= function(){ 
+             if(this.status === 200){
+               console.log(JSON.parse(xhr.responseText));
+             }
+           }
+           //enviar la peticion
+           xhr.send(datos);
         }
 
 }
