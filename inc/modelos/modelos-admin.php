c10ed56d34e74c611bd79e493396b24378f9cf3f
mjmf1 <marcomora61@gmail.com>
2021-07-06 17:27:45 -0500

    problema de recibimiento de datos solucionado

--- inc/modelos/modelos-admin.php
+++ inc/modelos/modelos-admin.php
@@ -1,30 +1,31 @@
 <?php
 //die(json_encode($_POST));
 
- $accion = $_POST('accion');
- $password = $_POST('password');
- $usuario = $_POST('usuario');
+ $accion = $_POST['accion'];
+ $password = $_POST['password'];
+ $usuario = $_POST['usuario'];
 
  if($accion === 'crear'){
      // codigo para crear los administardores 
 
-    /* //hashear password
+    //hashear password
      $opciones = array(
-         'cost' => 10
+         'cost' => 12
      );
-     //$hash_password = password_hash($password, PASSWORD_BCRYPT, $opciones);
+     $hash_password = password_hash($password, PASSWORD_BCRYPT, $opciones);
 
      $respuesta = array(
-        'pass' => $password
+        'pass' => $hash_password
      );
     
-    echo json_encode($respuesta);*/
+    echo json_encode($respuesta);
  }
 
  if($accion === 'login'){
 //     //escribir codigo que logee a los admin
-echo json_encode($_POST);
+//echo json_encode($_POST);
  }
 
- echo json_encode($_POST);
+ //echo json_encode($_POST);
  
+?>
