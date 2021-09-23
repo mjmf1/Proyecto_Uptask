7559add7b43c607893e84d95ee1af2b95b28056d
mjmf1 <marcomora61@gmail.com>
2021-09-22 22:12:01 -0500

    Agregando la Funcionalidad para Crear Nuevos Proyectos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
 eventListeners();
+//lista de proyectos
+var listaProyectos = document.querySelector('ul#proyectos');
 
 function eventListeners() {
     // boton para crear proyectos
@@ -8,5 +10,28 @@ function eventListeners() {
 }
 function nuevoProyecto(e){
     e.preventDefault();
-    console.log('presionaste nuevo proyecto');
+    //console.log('presionaste nuevo proyecto');
+
+    //crea un input para el nombre del nuevo proyecto
+    var nuevoProyecto = document.createElement('li');
+    nuevoProyecto.innerHTML = '<input type="text" id="nuevo-proyecto" >';
+    listaProyectos.appendChild(nuevoProyecto);
+
+    //selccionar el id con el nuevo proyecto
+    var inputNuevoProyecto = document.querySelector('#nuevo-proyecto');
+
+    // al presionar enter, crear en nuevo proyecto
+    inputNuevoProyecto.addEventListener('keypress', function(e) {
+        let tecla = e.which || e.keycode ;
+
+        if(tecla === 13){
+            guardaProyectosDB(inputNuevoProyecto.value);
+            listaProyectos.removeChild(nuevoProyecto);
+        }
+    });
+
+}
+
+function guardaProyectosDB(nombreProyecto){
+    console.log(nombreProyecto);
 }
